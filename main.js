8cbfd474eb6c2807cba357aecc23a21aa48f8d0a
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-10-15 12:04:29 -0500

    begin work

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // write your code here to make the tests pass
+
+var Library = function() {
+  
+};
+
+var Book = function() {
+  return {
+    getAttribute: function() {
+      return {
+        'checkedout': false
+      }
+    },
+    setAttribute: function() {
+      return {
+        'checkedout': false
+      }
+    }
+  }
+}
+
+var addBook = function() {
+}
